194bed2a5d312090024fe4fd72d4357edfdd828f
mikekibz59 <mikekibz59@gmail.com>
2020-12-09 10:50:26 +0300

    added api app and car registry app

--- sz_backend_challenge/settings.py
+++ sz_backend_challenge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8dfg+^o4nm4q=5ppj=3!ui+#3o7h-2=0@af*fj&rxaiwu)+%1f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'car_registry'
 ]
 
 MIDDLEWARE = [
